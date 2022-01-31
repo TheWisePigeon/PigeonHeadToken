22516ada3f717fbd659fa32fc6458b440e3a7037
TheWisePigeon <zozozozeph@gmail.com>
2022-01-31 21:12:48 +0000

    testing heroku

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-
 const express = require('express')
+const ipfs = require("ipfs")
 const bp = require('body-parser')
 const app = express()
+app.use(bp.urlencoded({ extended: true }))
+
+async function create() {
+    const node = await ipfs.create()
+}
+
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/pages/index.html" )
 
+})
+
+app.post('/', (req,res)=>{
+    let result = req.body
+    let name = result.name
+    let email = result.email
+    let password = result.password
+    let user = {
+        "name":`${name}`,
+        "password":`${password}`
+    }
+    res.send("Worked")
 })
 
 const port = 3000
