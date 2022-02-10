15da5585427e1fd716f6819f745e2ddeb2463249
TheWisePigeon <zozozozeph@gmail.com>
2022-02-10 08:46:35 +0000

    Update logic.js
    created functions to handle data adding to ipfs

--- logic.js
+++ logic.js
@@ -1,5 +1,21 @@
 const totalSupply = 10000
+const ipfs = require('ipfs')
+
+
+async function createNode() {
+    const node = await ipfs.create()
+    return node
+}
+
+async function addData(data) {
+
+    const node = await createNode()
+    const results = node.add(data)
+    return results
+}
 
 module.exports = {
-    totalSupply
+    totalSupply,
+    createNode,
+    addData
 }
