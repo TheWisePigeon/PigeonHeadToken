f0ece0de030a52f3715865f4159a5a68727c99d2
TheWisePigeon <zozozozeph@gmail.com>
2022-02-10 23:54:47 +0000

    Update firebase.js
    add app.initialize

--- firebase.js
+++ firebase.js
@@ -1,19 +1,29 @@
 const admin = require('firebase-admin')
+const { initializeApp } = require('firebase/app')
+const { getFunctions } = require('firebase/functions')
 var serviceAccount = require('./admin.json')
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-
+const app = initializeApp({
+    apiKey: "AIzaSyCXQ1MLdMT9Uy8nnRSeiRS5AFcKsaSuyJc",
+    authDomain: "pigeonhead-1d824.firebaseapp.com",
+    projectId: "pigeonhead-1d824"
 })
+admin.initializeApp({
+    credential : admin.credential.cert(serviceAccount)
+})
+console.log("g");
+const functions = getFunctions(app)
+console.log("h");
 
 var db = admin.firestore()
-var docRef = db.collection('Users').doc('new test').set({
+var docRef = db.collection('Users').doc('ne test').set({
     hash: "test"
 }).then(()=>{
     console.log("User created successfully :)");
 }).catch((error)=>{
     console.log("Error writing :" , error);
 })
+
 
 // docRef.get().then((doc) => {
 //     if (doc.exists) {
