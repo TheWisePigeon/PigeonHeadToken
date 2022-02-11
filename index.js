1b95cc6ac9eb3c3948fc582588f6c67a3f679d22
TheWisePigeon <zozozozeph@gmail.com>
2022-02-11 12:17:07 +0000

    minor update
    added tailwind.css to gitignore

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function User(name, pwd, balance) {
 
 
 app.get('/', async (req, res)=>{
-    //res.sendFile(__dirname + "/pages/index.html")
+    res.sendFile(__dirname + "/pages/index.html")
     const user = await logic.getData('QmeiUY5F4Bu3vdrWx3k2NeqApNtJWehXC8htaWeLWP9YPT')
     const userData = JSON.parse(user)
     console.log("username :" +userData.name);
