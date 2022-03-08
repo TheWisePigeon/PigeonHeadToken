9835d69f2d278fadc0cb01bd2df455e75f34da8d
TheWisePigeon <zozozozeph@gmail.com>
2022-03-08 21:26:02 +0000

    testing#1

--- firebase.js
+++ firebase.js
@@ -1,54 +1,50 @@
 const admin = require('firebase-admin')
 const keys = require('./keys')
-const { initializeApp } = require('firebase/app')
-const { getFunctions } = require('firebase/functions')
-const { httpsCallable } = require('firebase/functions')
+const {
+    initializeApp
+} = require('firebase/app')
+const {
+    getFunctions
+} = require('firebase/functions')
+const {
+    httpsCallable
+} = require('firebase/functions')
 var serviceAccount = require('./admin.json')
+const {
+    firestore
+} = require('firebase-admin')
+const {
+    async
+} = require('@firebase/util')
 
 const app = initializeApp({
     apiKey: keys.apiKey,
     authDomain: keys.authDomain,
     projectId: keys.projectId
 })
-// admin.initializeApp({
-//     credential : admin.credential.cert(serviceAccount)
-// })
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+
 const functions = getFunctions(app)
 const hi = httpsCallable(functions, 'sayHello')
-// hi().then((result)=>{
-//     console.log(result);
-// }).catch((error)=>{
-//     console.log(error.code, error.message, error.details);
-// })
 
 const test = httpsCallable(functions, 'test')
 
-test({hash:"hashhh"}).then((result)=>{
-    console.log(result);
-}).catch((error)=>{
-    console.log(error.code);
-})
+const createUser = httpsCallable(functions, 'createUser')
+
+//get number of users registered
+
+async function getCount() {
+    const snapshot = await db.collection("Users").get()
+    const count = snapshot.size
+    return count
+}
 
 
-
-// var db = admin.firestore()
-// var docRef = db.collection('Users').doc('ne test').set({
-//     hash: "test"
-// }).then(()=>{
-//     console.log("User created successfully :)");
-// }).catch((error)=>{
-//     console.log("Error writing :" , error);
-// })
-
-
-// docRef.get().then((doc) => {
-//     if (doc.exists) {
-//         data = doc.data()
-//         console.log(data.hash);
-//     } else {
-//         // doc.data() will be undefined in this case
-//         console.log("No such document!");
-//     }
-// }).catch((error) => {
-//     console.log("Error getting document:", error);
-// });
+module.exports = {
+    count,
+    createUser
+}
--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const bp = require('body-parser')
 const logic = require('./logic')
 const ipfs = require('ipfs-http-client')
 const app = express()
+const { count } = require('./firebase')
+const { createUser } = require('./firebase')
 app.use(bp.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 //userSchema
-function User(name, pwd, balance) {
+function User(name, pwd, refCode) {
     this.name = name
     this.password = pwd
-    this.balance = balance
+    this.refCode = refCode
 }
 
 
@@ -30,11 +32,12 @@ app.get('/register', (req, res)=>{
 
 app.post('/register', async (req, res)=>{
     let result = req.body
-    let newUser = new User(result.name, result.pwd, 2000)
-    logic.totalSupply-=2000
-    const results = await logic.addData(JSON.stringify(newUser))
-    console.log(results);
+    let code = count+1
+    let newUser = new User(result.name, result.pwd, refCode)
 
+    const results = await logic.addData(JSON.stringify(newUser))
+    createUser({hash: results, refCode: code})
+    console.log("Process finished");
 })
 
 app.get('/login', (req, res)=>{
